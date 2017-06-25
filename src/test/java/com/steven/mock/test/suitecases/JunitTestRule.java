dcf7a63d1fffb2583bf8002d596f811f92561b8e
谦黄 <qq42289141@gmail.com>
2017-06-26 00:02:05 +0800

    补充完整rule示例

--- src/test/java/com/steven/mock/test/suitecases/JunitTestRule.java
+++ src/test/java/com/steven/mock/test/suitecases/JunitTestRule.java
@@ -1,10 +1,18 @@
 package com.steven.mock.test.suitecases;
 
+import org.junit.Rule;
+
 /**
  * @rule注解说明demo
  * @author Administrator
  *
  */
 public class JunitTestRule {
+	@Rule
+	public MyRule rule =new MyRule();
+}
 
+class MyRule {
+	
+	
 }
